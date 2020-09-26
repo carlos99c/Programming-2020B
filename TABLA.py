ea4604e70137a2ebd18db8ab42d545a37fd54031
carlos coral <ccp8.3coralcarlos@gmail.com>
2020-09-25 22:56:29 -0500

    tabla

---
+++ TABLA.py
@@ -0,0 +1,19 @@
+# script Se requiere de un programa que solicite al usuario el número de una tabla de
+#multiplicar, y visualice dicha tabla con la serie del 1 al 10. Nota: NO debe usar ninguna
+#expresión que no se haya trabajado en clase.
+"tabla de multiplicar de numero 2"
+"tabla_2"
+T1= ("2*1 ES "),2*1
+
+T2=("2*2ES"),2*2
+T3=("2*3ES"),2*3
+T4=("2*4ES"),2*4
+T5=("2*5ES"),2*5
+T6=("2*6ES"),2*6
+T7=("2*7ES"),2*7
+T8=("2*8ES"),2*8
+T9=("2*9ES"),2*9
+T10=("2*10ES"),2*10
+
+
+print("la tabla del numero 2 es ",T1,T2,T3,T4,T5,T6,T7,T8,T9,T10)
