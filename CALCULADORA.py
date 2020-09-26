820e658b2cdd5901780270c7df0b55763fd870be
carlos coral <ccp8.3coralcarlos@gmail.com>
2020-09-25 21:27:00 -0500

    calculadora

---
+++ CALCULADORA.py
@@ -0,0 +1,14 @@
+#CALCULADORA
+#script se requieres dos numeros que permitan realizar las operaciones
+#basicas (suma, resta, multiplicacion , division)
+"suma"
+N1=5
+N2=6
+suma= N1+N2
+resta=N1-N2
+multiplicacion=N1*N2
+division=N1/N2
+print ("el resultado de la suma es ",suma)
+print ("el resultado de la resta es ",resta)
+print("el resultado de la multiplicaion es ",multiplicacion)
+print ("el resultado de la division es", division)
