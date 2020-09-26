89e4db02e27ac30eab1ae329d5bc9e3619227bd7
carlos coral <ccp8.3coralcarlos@gmail.com>
2020-09-26 17:30:54 -0500

    nombre edad

---
+++ Nombre_Edad2.py
@@ -0,0 +1,15 @@
+# script edad
+#Escriba que teniendo en cuenta el año actual, solicite a un usuario
+#por consola: el nombre y el año de nacimiento, y le muestre por pantalla un mensaje
+#indicando el nombre ingresado y la edad actual.
+
+Año=2020
+A_nacimiento=0
+Edad=0
+print ("ingrese su nombre")
+Nombre = input()
+print ("escriba su año de nacimiento")
+A_nacimiento=int (input())
+Edad = Año - A_nacimiento
+
+print ("su nombre es",Nombre ,"y su edad es ",Edad)
