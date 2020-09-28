474b430d13aa3bc2ad284266a8b9d03307455ddc
carlos coral <ccp8.3coralcarlos@gmail.com>
2020-09-27 19:40:42 -0500

    multiplicacion tabla

---
+++ multiplicacion TABLA.py
@@ -0,0 +1,15 @@
+#tablas de 1 al 10
+print ("numero de tabla de  a l 10")
+
+n= int(input("ingresa tu numero de tabla"))
+
+print (n,(" * 1 es ="),(n*1))
+print (n,(" * 2 es ="),(n*2))
+print (n,(" * 3 es ="),(n*3))
+print (n,(" * 4 es ="),(n*4))
+print (n,(" * 5 es ="),(n*5))
+print (n,(" * 6 es ="),(n*6))
+print (n,(" * 7 es ="),(n*7))
+print (n,(" * 8 es ="),(n*8))
+print (n,(" * 9 es ="),(n*9))
+print (n,(" * 10 es ="),(n*10))
