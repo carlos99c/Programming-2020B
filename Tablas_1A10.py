4c1f8496a354fde40a834b8f2ef5e0e00c9658ca
carlos coral <ccp8.3coralcarlos@gmail.com>
2020-09-27 19:29:14 -0500

    tablas de 1a10

---
+++ Tablas_1A10.py
@@ -0,0 +1,199 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+ingresa un numero5
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+ingresa un numero5
+ su numero es 10
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+Traceback (most recent call last):
+  File "D:/Carlos/PYTHON CODY/entrada de datos.py", line 7, in <module>
+    nombre= carlos
+NameError: name 'carlos' is not defined
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+hola carlos tienes 52 años
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+dame tu numeor del 1 al 10
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+dame tu numero5
+5
+10
+15
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla5
+5
+10
+15
+25
+30
+35
+40
+45
+50
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla 
+Traceback (most recent call last):
+  File "D:/Carlos/PYTHON CODY/entrada de datos.py", line 12, in <module>
+    n= int(input("dame tu numero de tabla"))
+ValueError: invalid literal for int() with base 10: ' '
+>>> 
+>>> 
+>>> 
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla5
+1
+Traceback (most recent call last):
+  File "D:/Carlos/PYTHON CODY/entrada de datos.py", line 14, in <module>
+    print("1") (n*1)
+TypeError: 'NoneType' object is not callable
+>>> 
+Warning (from warnings module):
+  File "D:/Carlos/PYTHON CODY/entrada de datos.py", line 14
+    print(("A1") (n*1))
+SyntaxWarning: 'str' object is not callable; perhaps you missed a comma?
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla2
+Traceback (most recent call last):
+  File "D:/Carlos/PYTHON CODY/entrada de datos.py", line 14, in <module>
+    print(("A1") (n*1))
+TypeError: 'str' object is not callable
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla6
+6
+12
+18
+30
+36
+42
+48
+54
+60
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla5
+1*1 es igual 
+Traceback (most recent call last):
+  File "D:/Carlos/PYTHON CODY/entrada de datos.py", line 14, in <module>
+    print ("1*1 es igual ")(n*1)
+TypeError: 'NoneType' object is not callable
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla6
+1*1 es igual 
+12
+18
+30
+36
+42
+48
+54
+60
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla2
+1*1 es = 
+4
+6
+10
+12
+14
+16
+18
+20
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla2
+1*1 es =  2
+4
+6
+10
+12
+14
+16
+18
+20
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla5
+5 *1 es =  5
+5 1*2 es =  10
+15
+25
+30
+35
+40
+45
+50
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla5
+5 x1 es =  5
+5 x2 es =  10
+5 x1 es =  15
+5 x1 es =  25
+5 x1 es =  30
+5 x1 es =  35
+5 x1 es =  40
+5 x1 es =  45
+5 x1 es =  50
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla9
+9 * 1 es =  9
+9 * 2 es =  18
+9 * 3 es =  27
+9 *  4 es = 36
+9 * 4 es =  45
+9 * 5 es =  54
+9 * 6es =  63
+9 * 7 es =  72
+9 * 8 es =  81
+9 * 9 es =  90
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+Traceback (most recent call last):
+  File "D:/Carlos/PYTHON CODY/entrada de datos.py", line 1, in <module>
+    opera
+NameError: name 'opera' is not defined
+>>> 
+============== RESTART: D:/Carlos/PYTHON CODY/entrada de datos.py ==============
+tabla de 1 al 10
+dame tu numero de tabla8
+8 * 1 es =  8
+8 * 2 es =  16
+8 * 3 es =  24
+8 *  4 es = 32
+8 * 4 es =  40
+8 * 5 es =  48
+8 * 6es =  56
+8 * 7 es =  64
+8 * 8 es =  72
+8 * 9 es =  80
+>>> 
