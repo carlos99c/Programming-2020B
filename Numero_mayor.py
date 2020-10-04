a5e22d0c76d967e531f379d677bc0a21d1e2e295
carlos coral <ccp8.3coralcarlos@gmail.com>
2020-10-04 17:26:45 -0500

    numero mayor

---
+++ Numero_mayor.py
@@ -0,0 +1,17 @@
+""" se requiere de un programa que solicite dos numeros al usuario
+por consola y determine cual de los dos es mayor 
+"""
+#import os
+#os.system("cls")
+
+num1=0
+num2=0
+print  ("dame tu primer numero")
+num1 =int (input())
+print ("dame tu segundo numero")
+num2 = int (input ())
+if num1 > num2 :
+    print ("el  primer  numero es mayor")
+else :
+    print ("el segundo valor es mayor") 
+
---
+++ mayor2.py
@@ -0,0 +1,18 @@
+""" se requiere de un programa que solicite
+al usuario por consola y determine cual de los dos 
+es el mayor"""
+
+#import os
+#os.sysitem("cls")
+
+num1=0
+num2=0
+print ("programa para calcular el mayor numero")
+print ("digita tu primer numero")
+num1= int (input())
+print ("diguite su segundo numero ")
+num2= int (input())
+if num1>num2 :
+    print ("el primer numero es mayor")
+else :
+    print ("el segundo numero es mayor")    
